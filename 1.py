09c4b7f936d54bde1f56f1c10f44a75500793fda
szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-02 19:48:38 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,36 +5,36 @@
 
 # a) z - zmienna lokalna funkcji
 
-# def add_or_sub(*args):
-#     if define_action % 2 == 0:
-#         result_add = x + y
-#         print(result_add)
-#     else:
-#         result_sub = x - y
-#         print(result_sub)
-#
-# x = int(input('Podaj x: '))
-# y = int(input('Podaj y: '))
-#
-# define_action = int(input('Podaj liczbe, jeżeli chcesz dodawać wpisz parzystą, a jeśli odejmować nieparzystą: '))
-#
-# add_or_sub(x, y, define_action)
+def add_or_sub(*args):
+    if define_action % 2 == 0:
+        result_add = x + y
+        print(result_add)
+    else:
+        result_sub = x - y
+        print(result_sub)
+
+x = int(input('Podaj x: '))
+y = int(input('Podaj y: '))
+
+define_action = int(input('Podaj liczbe, jeżeli chcesz dodawać wpisz parzystą, a jeśli odejmować nieparzystą: '))
+
+add_or_sub(x, y, define_action)
 
 # b) z - zmienna globalna funkcji
 
-# define_action = 2
-#
-# def add_or_sub(*args):
-#     global define_action
-#
-#     if define_action % 2 == 0:
-#         result_add = x + y
-#         print(result_add)
-#     else:
-#         result_sub = x - y
-#         print(result_sub)
-#
-# x = int(input('Podaj x: '))
-# y = int(input('Podaj y: '))
-#
-# add_or_sub(x, y, define_action)
+define_action = 2
+
+def add_or_sub(*args):
+    global define_action
+
+    if define_action % 2 == 0:
+        result_add = x + y
+        print(result_add)
+    else:
+        result_sub = x - y
+        print(result_sub)
+
+x = int(input('Podaj x: '))
+y = int(input('Podaj y: '))
+
+add_or_sub(x, y, define_action)
