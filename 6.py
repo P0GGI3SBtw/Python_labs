8d41a18429b0a0174fdf00699967e5e4b2787fcf
szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-02 22:30:19 +0200

    done

--- 6.py
+++ 6.py
@@ -1,22 +1,44 @@
 ##### Zadanie 6
-# Utwórz dwie funkcje, które sprawdzą czy
-# dany wyraz jest palindromem (wyrażenie brzmiące tak samo czytane od strony lewej
-# do prawej i od prawej do lewej np. kajak)
-# czy 2 dane wyrazy są anagramami (burza, arbuz)
+# Sprawdza czy dany wyraz jest palindromem (wyrażenie brzmiące tak samo
+# czytane od strony lewej do prawej i od prawej do lewej np. kajak)
 
-def palindrom(word):
-    i = 1
-    j = len(word)
-    if i < j:
-        if word[i] == word[j]:
-            i += 1
-            j += 1
+def palindr(word):
+    x = 0
+    for i in range(len(word)):
+        if word[0] == word[-1]:
+            x = x + 1
         else:
-            print('wyraz nie jest palindomem')
+            x = x
+
+    if len(word) == x:
+        print('Podany wyraz jest palindorem')
     else:
-        print('wyraz jest palindomem')
+        print('Podany wyraz nie jest palindorem')
 
+word = input('Podaj wyraz, który chcesz sprawdzić: ')
 
-word = input('Podaj wyraz: ')
+palindr(word)
 
-palindrom(word)
+# Sprawdza czz 2 dane wyrazy są anagarmami, wyrazy, które powstały z tych samych liter np. (arbuz, burza)
+
+def anagram(word1, word2):
+    lst_w1 = []
+    lst_w2 = []
+
+    for i in range(len(word1)):
+        lst_w1.append(i)
+    lst_w1.sort()
+
+    for i in range(len(word2)):
+        lst_w2.append(i)
+    lst_w2.sort()
+
+    if lst_w1 == lst_w2:
+        print('Podane wyrazy są anagramami')
+    else:
+        print('Podanie wyrazy nie są anagramami')
+
+word1 = input('Podaj pierwszy wyraz: ')
+word2 = input('Podaj drugi wyraz: ')
+
+anagram(word1, word2)
