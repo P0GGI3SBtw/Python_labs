328d70a15a813f67d14d2d271aab3d90b8674aa5
P0GGI3SBtw <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-07 12:47:22 +0200

    done

--- 1/functions/info.py
+++ 1/functions/info.py
@@ -19,7 +19,6 @@ def personal_data(imie, nazwisko, lat, mieszka, zonaty, karany):
 def family_data(imie_zony, nazwisko_zony, liczba_dzieci, lst_dzieci):
     """
     :parameter
-<<<<<<< HEAD
         lokalne:
             imie_zony (str)
             nazwisko_zony (str)
@@ -29,30 +28,3 @@ def family_data(imie_zony, nazwisko_zony, liczba_dzieci, lst_dzieci):
     """
     print(f'Imie żony: {imie_zony}, nazwisko żony: {nazwisko_zony}, '
           f'liczba dzieci: {liczba_dzieci}, imiona dzieci: {lst_dzieci}')
-=======
-        zonaty (str)
-        imie żony (str)
-        nazwisko żony (str)
-        liczna dzieci (int)
-    :return
-        Jeśli jest zonaty, to pyta, o żone oraz dzieci i ich imiona,
-        a jeśli nie to nic nie wykonuje
-    """
-
-    if zonaty == 'tak':
-        imie_zony = input('Podaj imie żony: ')
-        nazwisko_zony = input('Podaj nazwisko żony: ')
-        liczba_dzieci = int(input('Podaj liczbe dzieci: '))
-
-        list_dzieci = []
-
-        for i in range(liczba_dzieci):
-            imie_dziecka = input('Podaj imie dziecka: ')
-            list_dzieci.append(imie_dziecka)
-
-        print(f'Imie żony: {imie_zony}, nazwisko żony: {nazwisko_zony}, '
-              f'liczba dzieci: {liczba_dzieci}, imona dzieci: {list_dzieci}')
-    else:
-        pass
-    
->>>>>>> 7b92f1d9e791434cf5c1aa991e09289c5c04b9fd
