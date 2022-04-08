5973ba85d724f28177553b363cff914575ea4bc7
P0GGI3SBtw <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-08 11:26:54 +0200

    Update main.py

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,4 @@
-from functions.info import *
+from functions.info2 import *
 from functions import globals as gl
 
 gl.imie = imie = 'Jan'
@@ -12,16 +12,21 @@ karany = input('Podaj czy osoba była karana: ')
 personal_data(imie, nazwisko, lat, mieszka, zonaty, karany)
 
 if zonaty == 'tak':
-    imie_zony = input('Podaj imie żony: ')
+    imie_zony = input('Podaj imię żony: ')
     nazwisko_zony = input('Podaj nazwisko żony: ')
     liczba_dzieci = int(input('Podaj liczbę dzieci: '))
 
     lst_dzieci = []
 
-    for i in range(liczba_dzieci):
-        dzicko = input('Podaj imie dziecka: ')
-        lst_dzieci.append(dzicko)
-        i += 1
+    ## INFO
+    # for i in range(liczba_dzieci):
+    #     dzicko = input('Podaj imię dziecka: ')
+    #     lst_dzieci.append(dzicko)
+    #     i += 1
+
+    ## INFO 2
+
+    
 
     family_data(imie_zony, nazwisko_zony, liczba_dzieci, lst_dzieci)
 else:
