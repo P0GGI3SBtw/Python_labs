5117e7ae23cdb736369e77e78e301a48b073b3ac
P0GGI3SBtw <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-08 17:39:13 +0200

    done

---
+++ 1/functions/info2.py
@@ -0,0 +1,51 @@
+"""info2.py: Mouduł zawiera funkcję do pliku main.py, pozwalające na jego obsłge"""
+
+__name__ = 'info2'
+__author__ = 'Szymon Kołodziejski'
+__version__ = '1.0.0'
+
+
+def personal_data(imie, nazwisko, lat, mieszka, zonaty, karany):
+    """
+    Args:
+        global:
+            :param imie: (str)
+            :param nazwisko: (str)
+        lokal:
+            :param lat: (int)
+            :param mieszka: (str)
+            :param zonaty: (str)
+            :param karany: (str)
+    Returns:
+        Zdanie wypełnione zmiennymi (str, int)
+    Raises:
+        Zaimplementowany wyjątek zapobiega wprowadzeniu przez użytkownika złego typy zmiennej w pliku main.py
+    """
+    while True:
+        try:
+            print('Imie: {}, nazwisko: {}, lat: {}, mieszka: {} zonaty: {}, karany: {}'.format(imie, nazwisko, lat,
+                                                                                               mieszka, zonaty, karany))
+            break
+        except ValueError:
+            print('Podano parametr niewłaściwego typu')
+
+
+def family_data(imie_zony, nazwisko_zony, liczba_dzieci, **lst_dzieci):
+    """
+    Args:
+        :param imie_zony: (str)
+        :param nazwisko_zony: (str)
+        :param liczba_dzieci: (int)
+        :param kwargs: (str, list), (imiona dzieci)
+    Returns:
+        Zdanie wypełnione zmiennymi (str, int)
+    """
+    while True:
+        try:
+            print('Imie żony: {}, nazwisko żony: {}, '
+                  'liczba dzieci: {}, imiona dzieci:'.format(imie_zony, nazwisko_zony, liczba_dzieci))
+            for key, value in lst_dzieci.items():
+                print('{}: {}'.format(key, value))
+            break
+        except ValueError:
+            print('Podano parametr niewłaściwego typu')
