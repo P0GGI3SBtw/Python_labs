ddd2823735f3ca73e3a42028d48c5de7c443718b
Szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-05-13 11:21:37 +0200

    done

--- 1.py
+++ 1.py
@@ -1,31 +1,30 @@
 from tkinter import *
 
-app = Tk()
-app.geometry('300x150')
-
 
 def add():
     try:
-        result = lambda int1, int2: int(int1) + int(int2)
-        l_result.config(text='Result of action is : {}'.format(result(integer1.get(), integer2.get())))
-    except ValueError:
-        print('To nie jest cyfra!')
+        result = int(integer1.get()) + int(integer2.get())
+        result_l.config(text='Result of action is: {}'.format(result))
 
 
-l1 = Label(app, text='Podaj pierwszą liczbę:')
-l1.grid(row=1, column=1)
+
+app = Tk()
+app.title('my app')
+app.geometry('250x200')
+
+Label(text='Entry first integer: ').grid(row=1, column=1)
+
 integer1 = Entry(app, fg='blue')
 integer1.grid(row=1, column=2)
 
-l2 = Label(app, text='Podaj drugą liczbę:')
-l2.grid(row=2, column=1)
+Label(text='Entry second integer: ').grid(row=2, column=1)
+
 integer2 = Entry(app, fg='blue')
 integer2.grid(row=2, column=2)
 
-btn_ok = Button(app, text='OK', command=add)
-btn_ok.grid(row=3, column=2)
+Button(text='OK', command=add).grid(row=3, column=1)
 
-l_result = Label(app, text='Result of action is: 0')
-l_result.grid(row=4, column=2)
+result_l = Label(text='Place for result')
+result_l.grid(row=3, column=2)
 
 app.mainloop()
